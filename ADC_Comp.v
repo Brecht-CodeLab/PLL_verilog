9e53eb369825f5da3def302fb9b68914e6b4fc10
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-03-24 15:09:30 +0100

    real adc speed

--- ADC_Comp.v
+++ ADC_Comp.v
@@ -7,15 +7,26 @@ module ADC_Comp (
     input wire [11:0] ADC,
     output reg ADC_comp
 );
+    reg [11:0] ADC_reg = 0;
+    reg [8:0] counter = 9'h190;
+
     always @(posedge clk) begin
         if(~nrst || ~swiptAlive)begin
+            ADC_reg <= 0;
             ADC_comp <= 0;
         end
-        else if (ADC > 12'h7FF) begin
-            ADC_comp <= 0;
+        if(ADC >= 0)begin
+            ADC_reg <= ADC;
         end
-        else if (ADC < 12'h800) begin
-            ADC_comp <= 1;
+        
+        if(counter == 0)begin
+            counter <= 9'h190;
+            if (ADC_reg > 12'h7FF) begin
+                ADC_comp <= 0;
+            end
+            else if (ADC_reg < 12'h800) begin
+                ADC_comp <= 1;
+            end
         end
     end
 endmodule
