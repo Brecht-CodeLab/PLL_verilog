c449bf35f64842538df826b9d3e1dfdb92c78938
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-03-29 13:38:09 +0200

    K 10 khz

--- PLL2/PLL2.v
+++ PLL2/PLL2.v
@@ -14,7 +14,7 @@ module PLL2 (
     reg [31:0] period, half_period, cnt;
     reg [31:0] phase_error, pulse_length;
     reg [31:0] f0 = 32'h9C40; //41kHz
-    reg [31:0] delf = 32'h9C40; //15kHz
+    reg [31:0] delf = 32'h2710; //10kHz
     initial begin
         vco =  0;
         f = f0;
